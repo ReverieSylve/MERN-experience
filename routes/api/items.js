841ab077fc1c1b9dae8d4380cb2921fa60c748b6
Reverie <patlavovach@gmail.com>
2021-05-28 20:08:29 +0300

    why???

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Item = require('../../models/item');
+const Item = require('../../models/Item');
 
 router.get('/', (request, response) => {
   Item.find()
